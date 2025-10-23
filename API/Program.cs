41c2b6cb44f051ac0665697369fe0b6c684c6369
MurilloNFaria <nascimentomurillo24@gmail.com>
2025-10-23 18:45:53 -0300

    feat: User login and registration from the client side

--- API/Program.cs
+++ API/Program.cs
@@ -36,7 +36,7 @@ var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod()
-    .WithOrigins("http://localhost:4200", "https://localhost:4200"));
+    .AllowAnyOrigin());
 
 app.UseAuthentication();
 app.UseAuthorization();
